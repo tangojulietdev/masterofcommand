6ccc1cb9038f55ea70faf9ac7204a3618ff4834d
tangojulietdev <atjv88@gmail.com>
2026-06-14 03:33:35 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'moc-v20';
+const CACHE = 'moc-v29';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
